3eedc3f4819dc4910264061ad5411e53a07a9a1c
merlyle <merlyle@a9a46f4b-810a-8dbb-91c9-bb99b4252c80>
2010-06-20 04:00:45 +0000

    转到clanlib下的第一个版本，目前内存申请有问题。

---
+++ trunk/mspse-svn/src/app/TeapoyApp.cpp
@@ -0,0 +1,101 @@
+﻿#include "teapoyapp.h"
+
+// Setup clanlib modules:
+CL_SetupCore		setup_core;
+CL_SetupDisplay		setup_display;
+CL_SetupGL			setup_gl;
+
+int main(const std::vector<CL_String> &args)
+{
+	TeapoyApp teapoyApp;
+	teapoyApp.OnExecute();
+
+	return 0;
+}
+
+CL_ClanApplication app(&main);
+
+
+TeapoyApp::TeapoyApp()
+{
+	void* mp = CL_StaticMemoryPool.alloc(sizeof(CL_ConsoleWindow)*10);
+
+	console = cl_new(sizeof(CL_ConsoleWindow),mp) CL_ConsoleWindow;
+
+
+
+
+}
+
+int TeapoyApp::drawSth(CL_GraphicContext gc)
+{
+
+
+	CL_Draw::line(gc, 0, 200, 640, 200, CL_Colorf::yellow);
+	CL_Draw::line(gc, 200, 0, 200, 300, CL_Colorf::yellow);
+
+	CL_String str = cl_text("abc中文经测试中文可以使用");
+	//font.draw_text(gc, 100, 100, str, CL_Colorf::black);
+
+
+	CL_Rect re = CL_Rect(-5,-5,5,5);
+	CL_PixelBufferRef pixelBuf = gc.get_pixeldata(re);
+	gc.draw_pixels(200,200,30,20, pixelBuf, CL_Colorf::red);
+
+
+	CL_Vec4f red_color(1.0f, 0.0f, 0.0f, 1.0f);
+	CL_Vec4f green_color(0.0f, 1.0f, 0.0f, 1.0f);
+
+	CL_Vec2i positions[] = { CL_Vec2i(10,10), CL_Vec2i(100,100) };
+	CL_Vec4f colors[] = { red_color, green_color };
+
+	//CL_PrimitivesArray vertices(gc);
+	//vertices.set_attribute(0, positions);
+	//vertices.set_attribute(1, colors);
+
+	//gc.set_program_object(cl_program_color_only);
+	//gc.draw_primitives(cl_lines, 2, vertices);
+
+	return 0;
+}
+
+int TeapoyApp::OnExecute()
+{
+
+	OnInit();
+
+	try
+	{
+		while (!keyboard.get_keycode(CL_KEY_ESCAPE))
+		{
+			// Draw some text and lines:
+			gc.clear(CL_Colorf::cadetblue);
+
+			drawSth(gc);
+
+			// Make the stuff visible:
+			window.flip();
+
+			// Read messages from the windowing system message queue,
+			// if any are available:
+			CL_KeepAlive::process();
+
+			// Avoid using 100% CPU in the loop:
+			CL_System::sleep(10);
+		}
+	}
+	catch(CL_Exception &exception)
+	{
+		// Create a console window for text-output if not available
+
+		CL_Console::write_line("Exception caught: " + exception.get_message_and_stack_trace());
+		console->display_close_message();
+
+		return -1;
+	}
+
+	return 0;
+}
+
+
+
---
+++ trunk/mspse-svn/src/app/TeapoyApp.h
@@ -0,0 +1,61 @@
+﻿#ifndef _TEAPOYAPP_H_
+#define _TEAPOYAPP_H_
+
+//clanlib引用
+#include <ClanLib/core.h>
+#include <ClanLib/display.h>
+#include <ClanLib/gl.h>
+#include <ClanLib/application.h>
+
+
+
+//app类
+
+class TeapoyApp
+{
+private:
+
+	//app类初始化
+	CL_Font				font;
+	CL_ConsoleWindow*	console;
+
+	//oninit过程初始化
+	CL_DisplayWindow	window;
+	CL_GraphicContext	gc;
+	CL_InputDevice		keyboard;
+
+
+public:
+	TeapoyApp();
+
+
+	static int drawSth(CL_GraphicContext gc);
+
+	int OnExecute();
+
+public:
+
+	/** 游戏主过程之初始化 */
+	int OnInit();
+
+	/** 游戏主过程之事件处理 */
+	int OnEvent();
+
+	/** 游戏主过程之主循环 */
+	int OnMainLoop();
+
+	/** 游戏主过程之渲染 */
+	int OnRender();
+
+	/** 游戏主过程之清理 */
+	int OnCleanup();
+
+	/** 程序渲染线程循环函数 */
+	int RenderThreadLoop();
+
+
+	/** 用户发出退出请求时调用 */
+	int OnExit();
+};
+
+#endif
