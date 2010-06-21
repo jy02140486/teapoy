b8add67df2d10430320e86477eb8dcc164ebd517
merlyle <merlyle@a9a46f4b-810a-8dbb-91c9-bb99b4252c80>
2010-06-21 13:13:39 +0000

    teapoy-svn起始版本。（修正）

---
+++ teapoy-svn/src/app/init.cpp
@@ -0,0 +1,37 @@
+#include "app.h"
+
+bool T_App::init()
+{
+	try
+	{
+	//initail window description
+	mWinDesc.set_title("Teapoy");
+	mWinDesc.set_allow_resize(true);
+	mWinDesc.set_size(CL_Size (800, 600), false);
+	//initail gui
+	mpDisplayWindow = new CL_DisplayWindow(mWinDesc);
+	mpWinManager = new CL_GUIWindowManagerTexture(*mpDisplayWindow);
+	mGui.set_window_manager(*mpWinManager);
+
+	//binding close signal to displayWindow
+	CL_Slot slot = mpDisplayWindow->sig_window_close().connect(this, &T_App::closeWindow);
+
+	//initail console window
+	mpConsole = new CL_ConsoleWindow("Console", 80, 100);
+
+	//initail events
+	mInput = mpDisplayWindow->get_ic();
+	mKeyboard = mInput.get_keyboard();
+	mMouse = mInput.get_mouse();
+	//mJoystick = mInput.get_joystick();
+	return true;
+	}
+	catch (CL_Exception &exception)
+	{
+		CL_Console::write_line("Exception", exception.get_message_and_stack_trace());
+		mpConsole->display_close_message();
+		return false;
+	}
+}
+
+
