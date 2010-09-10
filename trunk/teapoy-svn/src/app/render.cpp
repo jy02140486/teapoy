ecb3f2b8210ca0e4a76b742315b3aa03e2eb3da1
jy02140486@sina.com <jy02140486@sina.com@a9a46f4b-810a-8dbb-91c9-bb99b4252c80>
2010-09-10 08:21:01 +0000

    Animated sky added

--- trunk/teapoy-svn/src/app/render.cpp
+++ trunk/teapoy-svn/src/app/render.cpp
@@ -3,6 +3,9 @@
 void T_App::render()
 {
 	mpDisplayWindow->get_gc().clear(CL_Colorf::yellowgreen);
+
+	bgr->draw(&mpDisplayWindow->get_gc());
+
  	mpphytester->draw(mpDisplayWindow->get_gc());
 
 	if (addground->isActivated())
@@ -10,8 +13,8 @@ void T_App::render()
 		addground->draw(&mpDisplayWindow->get_gc());
 	}
 	
-	bgr->draw(&mpDisplayWindow->get_gc());
 	
 	mpWinManager->draw_windows(mpDisplayWindow->get_gc());
+	
 	mpDisplayWindow->flip();
 }
