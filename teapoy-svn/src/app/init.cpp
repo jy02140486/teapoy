d8ed39b1d0c31fb069b2cd1904a0909fde6c964f
jy02140486@sina.com <jy02140486@sina.com@a9a46f4b-810a-8dbb-91c9-bb99b4252c80>
2010-09-16 04:16:05 +0000

    Fps limited. Some frequency of physics updating problem still existed.

--- teapoy-svn/src/app/init.cpp
+++ teapoy-svn/src/app/init.cpp
@@ -1,5 +1,6 @@
 #include "app.h"
 #include "event.h"
+#include <time.h>
 
 bool T_App::init()
 {
@@ -146,9 +147,9 @@ bool T_App::init()
 
 		slider_vertical = new CL_Slider(mpComWindow);
 		slider_vertical->set_geometry(CL_Rect(40, 130, CL_Size(200, 20)));
-		slider_vertical->set_max(240);
-		slider_vertical->set_min(0);
-		slider_vertical->set_position(240);
+		slider_vertical->set_max(120);
+		slider_vertical->set_min(1);
+		slider_vertical->set_position(120);
 
 		slider_vertical->func_value_changed().set(this,&T_App::OnSliderMove)	;
 
@@ -183,6 +184,7 @@ bool T_App::init()
 	
 	bgr->Init(&(mpDisplayWindow->get_gc()));
 
+	time(&Atime);
 	}
 	catch (CL_Exception &exception)
 	{
